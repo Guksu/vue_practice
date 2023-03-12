6408e6929f2d80629fd3957a5ec91cd1013d23e0
Guksu <yung3152@daum.net>
2023-03-12 17:58:01 +0900

    [2023-03-12]
    1. vue 기초 문법

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="menu">
+    <!-- 반복문 , js의 map과 유사 -->
+    <a v-for="name in menu" :key="name">{{ name }}</a>
+  </div>
+  <div v-for="(item,i) in products" :key="i">
+    <h4>{{ item }} 원룸</h4>
+    <button v-on:click="btnClick">허위매물신고</button><span>신고수 : {{reportNum}}</span>
+  </div>
+    <div v-if="modal">true!!!</div>
+    <div v-else>false!!!</div>
+
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { defineComponent } from 'vue';
 
-export default {
+export default defineComponent({
   name: 'App',
+  data(){
+    return{
+        modal:true,
+        reportNum : 0,
+        products : ["양천구", "강서구"],
+        menu : ["Home", "Products", "About"]
+    }
+  },
+  methods:{
+      btnClick(){
+        this.reportNum++;
+        this.modal = !this.modal;
+      }
+  },
   components: {
-    HelloWorld
   }
-}
+});
 </script>
 
 <style>
@@ -22,5 +44,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.menu{
+  background-color: wheat;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a{
+  color : darkslateblue;
+  padding: 10px;
 }
 </style>
